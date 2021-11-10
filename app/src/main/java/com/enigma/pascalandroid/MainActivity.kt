384e53ebb4257c3ea09a04874bd36f0142d79947
rifqyram <rifqyomp@gmail.com>
2021-11-10 14:39:11 +0700

    [HANDSON] counter app

--- app/src/main/java/com/enigma/pascalandroid/MainActivity.kt
+++ app/src/main/java/com/enigma/pascalandroid/MainActivity.kt
@@ -1,30 +1,48 @@
 package com.enigma.pascalandroid
 
-import android.graphics.Color
-import android.os.Build
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.core.view.WindowCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var count: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT in 21..29) {
-            window.statusBarColor = Color.TRANSPARENT
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.decorView.systemUiVisibility =
-                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_STABLE
+        count = findViewById(R.id.textView)
+        val btnIncrement: MaterialButton = findViewById(R.id.btn_increment)
+        val btnDecrement: MaterialButton = findViewById(R.id.btn_decrement)
+        btnIncrement.setOnClickListener { increment() }
+        btnDecrement.setOnClickListener { decrement() }
 
-        } else if (Build.VERSION.SDK_INT >= 30) {
-            window.statusBarColor = Color.TRANSPARENT
-            // Making status bar overlaps with the activity
-            WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    private fun increment() {
+        val currentNumber = count.text.toString().toInt()
+        count.text = (currentNumber + 1).toString()
+    }
+
+    private fun decrement() {
+        val currentNumber = count.text.toString().toInt()
+
+        if (currentNumber <= 0) {
+            count.text = "0"
+        } else {
+            count.text = (currentNumber - 1).toString()
         }
     }
 }
